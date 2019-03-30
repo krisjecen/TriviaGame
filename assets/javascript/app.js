59aff823a2f04ab56cbe6f2975740791e3f718ea
On Kilter <13891670+krisjecen@users.noreply.github.com>
2019-03-29 23:48:36 -0700

    set up basic variables and established initial timer structure, to be refined

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,12 @@
 // js for TriviaGame
 
 // variables / objects
+var questionTimer = 3;
+var questionTimerRunning = false;
+var nthQuestion = 0;
+
+
+
 // should i make an object for the set of questions?
 // for example
 
@@ -25,6 +31,12 @@ var questions = {
     },
 };
 
+var qCorrect = 0; // initial value for # of Qs the user has answered correctly
+var qIncorrect = 0; // initial value for # of Qs the user has answered incorrectly
+var questionsCount = Object.keys(questions);
+var qUnanswered = questionsCount.length; // initial value for # of Qs the user has not answered
+console.log(qUnanswered);
+
 console.log(questions.q1.text); // logs correctly "what color is the sky?"
 console.log(questions.q1.choices.incorrect2); // logs correctly "checkerboard"
 
@@ -32,6 +44,32 @@ console.log(questions.q1.choices.incorrect2); // logs correctly "checkerboard"
 
 // timer at the top of the page
 // gets reset on each new question
+document.getElementById("questionTimer").textContent = `Time remaining: ${questionTimer}`;
+
+
+
+questionIntervalID = setInterval(decrementQT, 1000);
+
+// should i make general start / stop timer functions and pass in a value for their countdown times?
+// since the values will probably be different for question vs answer timing
+
+function stopQT() {
+    questionTimerRunning = false;
+    clearInterval(questionIntervalID);
+    questionTimer = 3;
+}
+
+function decrementQT() {
+    if (questionTimerRunning === true) {
+        document.getElementById("questionTimer").textContent = `Time remaining: ${questionTimer}`;
+        questionTimer--;
+        if (questionTimer === 0) {
+            stopQT();
+        }
+    }
+    
+}
+
 
 // tiny TriviaGame:
     // one question, one answer choice, and timer
@@ -41,11 +79,74 @@ console.log(questions.q1.choices.incorrect2); // logs correctly "checkerboard"
 // likely functions:
 //
 // function to display a new question (initializing)
-// starts the timer at X seconds
+
+function triviaQuestion() {
+    // starts the timer at X seconds
+    questionTimer = 3;
+    questionTimerRunning = true;
+    setInterval(decrementQT, 1000);
+    nthQuestion += 1;
+    document.getElementById("questionCount").textContent = `Question ${nthQuestion} of ${questionsCount.length}`;
+    document.getElementById("triviaTextarea").textContent = `Here is the trivia question.`;
+    document.getElementById("triviaChoices").textContent = `Possible answer.`;
+
+    setTimeout(showAnswer, 4000);
+
+
+}
 // targets & updates question text
 // targets & updates answer choices
 
 // function to display the correct answer for the previous question
 //
+function showAnswer() {
+    // stop the timer
+    stopQT()
 
+    
+    // if the user selected the correct answer
+    // display "good job!" along with additional info
+    // decrement qUnanswered
+
+    // else if the user selected an incorrect answer
+    // display "incorrect,..." along with additional info
+    // decrement qUnanswered
+
+    // else if the user ran out of time
+    // display "out of time" message along with correct answer & additional info
+
+    // start countdown to next question
+    // leaving this here because we will change it into a separate answer timer function that will get called
+    // within this showAnswer function
+    questionTimer = 3;
+    questionTimerRunning = true;
+    setInterval(decrementQT, 1000);
+    document.getElementById("questionTimer").textContent = `Next question in: ${questionTimer}`;
+
+
+    // display the answer to the previous question -- should look different for correct vs incorrect response
+    document.getElementById("questionCount").textContent = "";
+    document.getElementById("triviaTextarea").textContent = `Here is the answer.`;
+    document.getElementById("triviaChoices").textContent = "";
+
+    // check if there aren't anymore questions to ask
+    function lastQuestioncheck() {
+        if (nthQuestion === questionsCount.length) {
+            displayUserStats()
+        } else {
+            triviaQuestion()
+        }
+    }
+    setTimeout(lastQuestioncheck, 4000);
+}
+
+function displayUserStats() {
+    stopQT()
+
+    // button for gameresults page -- still need to code it to reset the game tho!
+    document.getElementById("playAgain").style.display = "block";
+}
+
+triviaQuestion();
 // on-click events for each answer choice
+
